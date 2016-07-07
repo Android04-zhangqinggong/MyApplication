928582c4db46a5a8586d18cf29fd867fb492ae81
zhangqinggong <854627901@qq.com>
2016-07-07 17:51:14 +0800

    练习

--- myapplication4/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication4/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    // TODO: 2016/7/7  
+    Button btn;
+    // TODO: 2016/7/7
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
